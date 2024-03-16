0e2383be93daa318aba1c016c35694614cffa329
ParthSharma9993 <sparth332@gmail.com>
2024-03-16 22:41:39 +0530

    Completed Setup & Integration of Frontend

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,16 @@ const router = express.Router();
 
 //Routes
 router.get ('', (req,res) => {
-    res.send ("Hello World");
+    const locals ={
+        title: "Blog-Website-1.0",
+        description: "Simple Blog created with NodeJs, ExpressJs & MongoDB."
+    }
+    res.render('index', {locals});
+})
+
+
+router.get ('/about', (req,res) => {
+    res.render('about');
 })
 
 module.exports = router;
